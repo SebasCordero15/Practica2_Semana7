810e9eabee3bda5670e601a7bac41a4e69dc331a
Bengodie <dbenavides00218@ufide.ac.cr>
2024-03-19 20:43:36 -0600

    .

--- Practica2/src/practica2/Main.java
+++ Practica2/src/practica2/Main.java
@@ -37,6 +37,7 @@ public class Main {
       
         Supermercado superm = new Supermercado();
         ExecutorService ejecutor = Executors.newFixedThreadPool(5);
+        
         for (int i = 1; i <= 100; i++) {
            ejecutor.execute(new Cliente(i, superm));
         }
